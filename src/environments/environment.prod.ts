db1967a2286d3fa25861fdc667fdad208b3f941c
aanacif <aanacif@gmail.com>
2021-06-18 22:17:00 -0300

    new project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB_hifCjvJb7jf3hnZozfzlnJQ6N0iYuXI",
-    authDomain: "ionic5-firebase-authentication.firebaseapp.com",
-    databaseURL: "https://ionic5-firebase-authentication.firebaseio.com",
-    projectId: "ionic5-firebase-authentication",
+    apiKey: "AIzaSyDLvwjpuBuH6BCy3aD2ZPnFzTYheFYWbg8",
+    authDomain: "senacapp-d648b.firebaseapp.com",
+    databaseURL: "https://senacapp-d648b-default-rtdb.firebaseio.com",
+    projectId: "senacapp-d648b",
+    storageBucket: "senacapp-d648b.appspot.com",
+    messagingSenderId: "560452492596",
+    appId: "1:560452492596:web:fe6bcb9339ad25c3101961"
   }
 };
